62a633f136870e3c2b6f72e2eb926a6ddcecc2bf
adeadedoja <damdey@yahoo.com>
2018-08-24 01:52:59 +0200

    added contactt-create client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactCreateComponent
   ],
   imports: [
     BrowserModule
