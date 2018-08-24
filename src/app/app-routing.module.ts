8bbc8b717deafd4fd0ae86771949dad8296827e0
adeadedoja <damdey@yahoo.com>
2018-08-24 02:02:02 +0200

    routing working

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'contact', pathMatch: 'full' },
+    {
+        path: 'contact',
+        component: ContactCreateComponent
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ContactCreateComponent } from './contact-create/contact-create.component';
 
@@ -10,7 +12,8 @@ import { ContactCreateComponent } from './contact-create/contact-create.componen
     ContactCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
