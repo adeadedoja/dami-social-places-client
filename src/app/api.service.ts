c637525f3ceee5a9e94205b5b5fb76aa711649bb
adeadedoja <damdey@yahoo.com>
2018-08-26 23:31:15 +0200

    added https

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,7 +6,7 @@ import { catchError, tap, map } from 'rxjs/operators';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
-const apiUrl = "http://social.debonnairediaries.com";
+const apiUrl = "https://social.debonnairediaries.com";
 
 @Injectable({
   providedIn: 'root'
